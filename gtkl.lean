5e050100ae76d037e14ad74030951bf49e23c546
Joona Piirainen <joona.piirainen@gmail.com>
2022-09-18 20:02:13 +0300

    basic structures

--- gtkl.lean
+++ gtkl.lean
@@ -56,3 +56,63 @@ def volume (n₁ n₂ n₃ : ℕ) : ℕ :=
 def Str : Type := String
 
 def aStr : Str := "This is a string."
+
+structure Point where
+  point ::
+  x : Float
+  y : Float
+deriving Repr
+
+def origin : Point := { x := 0, y := 0 }
+
+#eval origin
+#eval origin.x
+#eval origin.y
+
+def addPoints (p₁ p₂ : Point) : Point :=
+  { x := p₁.x + p₂.x, y := p₁.y + p₂.y }
+
+def distance (p₁ p₂ : Point) : Float :=
+  let dx := (p₂.x - p₁.x) ^ 2
+  let dy := (p₂.y - p₁.y) ^ 2
+  (dx + dy).sqrt
+
+#eval distance {x := 1, y := 2} {x := 5, y := -1}
+
+structure Point3D where
+  x : Float
+  y : Float
+  z : Float
+deriving Repr
+
+def origin3D : Point3D := { x := 0.0, y := 0.0, z := 0.0 }
+
+def zeroX (p : Point) : Point :=
+  { p with x := 0 }
+
+def fourAndThree : Point :=
+  { x := 4, y := 3 }
+
+#eval zeroX fourAndThree
+#eval zeroX (Point.point 4 3)
+
+#check Point3D.mk
+
+#eval "one string".append " and another"
+
+structure RectangularPrism where
+  width : Float
+  height : Float
+  depth : Float
+
+def vol (r : RectangularPrism) : Float :=
+  r.width * r.height * r.depth
+
+structure Segment where
+  s : Point
+  e : Point
+
+def length (s : Segment) : Float :=
+  distance s.s s.e
+
+#eval length { s := { x := 1, y := 2 }, e := { x := 5, y := -1 } }
