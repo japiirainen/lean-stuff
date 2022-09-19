dceb42176f9770fbf2d5909f00cc4a25494fc67f
Joona Piirainen <joona.piirainen@gmail.com>
2022-09-19 18:14:03 +0300

    cat clone

--- feline/Main.lean
+++ feline/Main.lean
@@ -1,3 +1,38 @@
+def bufSize : USize := 20 * 1024
 
-def main : IO Unit :=
-  IO.println s!"Hello, cats!"
+partial def dump (stream : IO.FS.Stream) : IO Unit := do
+  let buf ← stream.read bufSize
+  if buf.isEmpty then
+    pure ()
+  else
+    let stdout ← IO.getStdout
+    stdout.write buf
+    dump stream
+
+def fileStream (filename : System.FilePath) : IO (Option IO.FS.Stream) := do
+  let fileExists ← filename.pathExists
+  if not fileExists then
+    (← IO.getStderr).putStrLn s!"File not found: {filename}"
+    pure none
+  else
+    let handle ← IO.FS.Handle.mk filename IO.FS.Mode.read
+    pure (some (IO.FS.Stream.ofHandle handle))
+
+def process (exitCode : UInt32) (args : List String) : IO UInt32 := do
+  match args with
+  | [] => pure exitCode
+  | "-" :: args =>
+    dump (← IO.getStdin)
+    process exitCode args
+  | filename :: args =>
+    let stream ← fileStream filename
+    match stream with
+    | none => process 1 args
+    | some stream =>
+      dump stream
+      process exitCode args
+
+def main (args : List String) : IO UInt32 := do
+  match args with
+  | [] => process 0 ["-"]
+  | _ => process 0 args
